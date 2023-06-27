1589f7709a1d0d41da97b2d2e106c109caad4355
ricky6362 <63843083+ricky6362@users.noreply.github.com>
2023-06-27 21:18:25 +0300

    Assignment done

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+function submitData(name, email){
+    const formData = {
+        name: name,
+        email: email
+    }
+
+    
+    configurationObject = {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            Accept: 'application/json' 
+        },
+        body: JSON.stringify(formData)
+    }
+        
+
+    fetch('http://localhost:3000/users', configurationObject)
+    .then(resp => resp.json())
+    .then(data =>{
+        console.log(data)
+    })
+    .catch(error => {
+        alert('Your code has an error.')
+        console.log(error.message)
+    })
+    
+}
